e560cb3f2fd32969c0b3fe3b7ac6ec21de9d95f0
Ericsen Tsai <bommer95175@gmail.com>
2022-06-28 17:17:50 +0800

    Add crew & technology page

--- client/src/pages/crew/Crew.jsx
+++ client/src/pages/crew/Crew.jsx
@@ -1,7 +1,94 @@
-import React from "react"
+import React, { useState } from "react"
+
+import "./Crew.scss"
+
+const crews = [
+  {
+    name: "Douglas Hurley",
+    images: {
+      png: "./images/crew/image-douglas-hurley.png",
+      webp: "./images/crew/image-douglas-hurley.webp",
+    },
+    role: "Commander",
+    bio:
+      "Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.",
+  },
+  {
+    name: "Mark Shuttleworth",
+    images: {
+      png: "./images/crew/image-mark-shuttleworth.png",
+      webp: "./images/crew/image-mark-shuttleworth.webp",
+    },
+    role: "Mission Specialist",
+    bio:
+      "Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.",
+  },
+  {
+    name: "Victor Glover",
+    images: {
+      png: "./images/crew/image-victor-glover.png",
+      webp: "./images/crew/image-victor-glover.webp",
+    },
+    role: "Pilot",
+    bio:
+      "Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer.",
+  },
+  {
+    name: "Anousheh Ansari",
+    images: {
+      png: "./images/crew/image-anousheh-ansari.png",
+      webp: "./images/crew/image-anousheh-ansari.webp",
+    },
+    role: "Flight Engineer",
+    bio:
+      "Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.",
+  },
+]
 
 const Crew = () => {
-  return <div>Crew</div>
+  const [crewInd, setCrewInd] = useState(0)
+
+  const handleOnClick = (e) => {
+    if (e.target.dataset.id === crewInd) return
+    setCrewInd(Number(e.target.dataset.id))
+  }
+
+  const renderList = () => {
+    return crews.map((crew, ind) => (
+      <div
+        className={`crew__option ${
+          crewInd === ind ? "crew__option--selected" : ""
+        }`}
+        data-id={ind}
+        onClick={handleOnClick}
+        key={crew.name}
+      ></div>
+    ))
+  }
+
+  return (
+    <div className="crew">
+      <div className="crew__info">
+        <div className="crew__info-title-box">
+          <h3 className="crew__info-title">
+            <span className="crew__info-number">02</span>
+            MEET YOUR CREW
+          </h3>
+        </div>
+        <h5 className="crew__role">{crews[crewInd].role}</h5>
+        <h1 className="crew__name">{crews[crewInd].name}</h1>
+        <p className="crew__bio">{crews[crewInd].bio}</p>
+        <div className="crew__option-box">{renderList()}</div>
+      </div>
+      <div className="crew__img-box">
+        <img
+          src={crews[crewInd].images.png}
+          alt={crews[crewInd].name}
+          className="crew__img"
+        />
+      </div>
+    </div>
+  )
 }
 
 export default Crew
--- client/src/pages/technology/Technology.jsx
+++ client/src/pages/technology/Technology.jsx
@@ -1,7 +1,99 @@
-import React from "react"
+import React, { useState } from "react"
+
+import "./Technology.scss"
+
+const technologies = [
+  {
+    name: "Launch vehicle",
+    images: {
+      portrait: "./images/technology/image-launch-vehicle-portrait.jpg",
+      landscape: "./images/technology/image-launch-vehicle-landscape.jpg",
+    },
+    description:
+      "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!",
+  },
+  {
+    name: "Spaceport",
+    images: {
+      portrait: "./images/technology/image-spaceport-portrait.jpg",
+      landscape: "./images/technology/image-spaceport-landscape.jpg",
+    },
+    description:
+      "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.",
+  },
+  {
+    name: "Space capsule",
+    images: {
+      portrait: "./images/technology/image-space-capsule-portrait.jpg",
+      landscape: "./images/technology/image-space-capsule-landscape.jpg",
+    },
+    description:
+      "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.",
+  },
+]
 
 const Technology = () => {
-  return <div>Technology</div>
+  const [technologyInd, setTechnologyInd] = useState(0)
+
+  const handleOnClick = (e) => {
+    console.log(e.target)
+
+    console.log(e.target.dataset.id)
+    if (e.target.dataset.id === technologyInd) return
+
+    setTechnologyInd(Number(e.target.dataset.id))
+  }
+
+  const renderList = () => {
+    return technologies.map((technology, ind) => (
+      <div
+        className={`technology__option ${
+          ind === technologyInd ? "technology__option--selected" : ""
+        }`}
+        key={technology.name}
+      >
+        <span
+          className="technology__option-index"
+          data-id={ind}
+          onClick={handleOnClick}
+        >
+          {ind + 1}
+        </span>
+      </div>
+    ))
+  }
+
+  return (
+    <div className="technology">
+      <div className="technology__info">
+        <div className="technology__info-title-box">
+          <h3 className="technology__info-title">
+            <span className="technology__info-number">03</span>
+            SPACE LAUNCH 101
+          </h3>
+        </div>
+        <div className="technology__content-box">
+          <div className="technology__option-box">{renderList()}</div>
+          <div className="technology__introduction">
+            <h5 className="technology__terminology">THE TERMINOLOGY...</h5>
+            <h1 className="technology__name">
+              {technologies[technologyInd].name}
+            </h1>
+            <p className="technology__description">
+              {technologies[technologyInd].description}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="technology__img-box">
+        <img
+          src={technologies[technologyInd].images.portrait}
+          alt={technologies[technologyInd].name}
+          className="technology__img"
+        />
+      </div>
+    </div>
+  )
 }
 
 export default Technology
