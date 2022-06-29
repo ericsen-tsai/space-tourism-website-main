9f622d32fe8ccc8979ebe56efec8b1c2a6cabae1
Ericsen Tsai <bommer95175@gmail.com>
2022-06-29 14:50:09 +0800

    Add RWD to each page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useRef, useEffect } from "react"
 import { Link } from "react-router-dom"
 
 import "./Navbar.scss"
@@ -6,6 +6,17 @@ import "./Navbar.scss"
 const pages = ["home", "destination", "crew", "technology"]
 
 const Navbar = ({ currentPage }) => {
+  const [isDisplay, setIsDisplay] = useState(false)
+  const navRef = useRef()
+  const hamburgerRef = useRef()
+
+  useEffect(() => {
+    const style = getComputedStyle(hamburgerRef.current)
+    if (style.display === "none") return
+    if (isDisplay) navRef.current.style.display = "inline-block"
+    if (!isDisplay) navRef.current.style.display = "none"
+  }, [isDisplay])
+
   const renderList = () => {
     return pages.map((page, ind) => (
       <li
@@ -33,6 +44,21 @@ const Navbar = ({ currentPage }) => {
       </div>
       <div className="navbar__nav">
         <ul className="navbar__list">{renderList()}</ul>
+      </div>
+      <div className="navbar__nav navbar__nav--mobile" ref={navRef}>
+        <ul className="navbar__list">{renderList()}</ul>
+        <div className="navbar__icon-box">
+          <i
+            className="navbar__icon navbar__icon--close"
+            onClick={() => setIsDisplay(false)}
+          ></i>
+        </div>
+      </div>
+      <div className="navbar__icon-box" ref={hamburgerRef}>
+        <i
+          className="navbar__icon navbar__icon--hamburger"
+          onClick={() => setIsDisplay(true)}
+        ></i>
       </div>
     </nav>
   )
